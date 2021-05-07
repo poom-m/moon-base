8d86625bafbc9c1e64f4ae3ed36ece1e854f8bab
Poom Medhavikul <dhanabhumi.m@cjworx.com>
2021-05-08 00:29:11 +0700

    Add template for history page

--- client/pages/history.vue
+++ client/pages/history.vue
@@ -1,9 +1,53 @@
 <template>
-  <div>History</div>
+  <b-container class="py-5">
+    <BlockSection class="balance-block">
+      <div class="balance-section">MOON left {{ moonBalance }} MOON</div>
+    </BlockSection>
+
+    <BlockSection>
+      <b-table-lite hover :items="orders" class="mb-0" />
+    </BlockSection>
+  </b-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      moonBalance: 1000,
+      orders: [
+        {
+          date_time: '2021-04-01 10:00:00',
+          ID: 'AAA',
+          THBT: 100,
+          MOON: 2,
+          RATE: '1 MOON = 50 THBT | 0.02',
+        },
+        {
+          date_time: '2021-04-01 10:00:00',
+          ID: 'BBB',
+          THBT: 100,
+          MOON: 2,
+          RATE: '1 MOON = 50 THBT | 0.02',
+        },
+        {
+          date_time: '2021-04-01 10:00:00',
+          ID: 'BBB',
+          THBT: 100,
+          MOON: 2,
+          RATE: '1 MOON = 50 THBT | 0.02',
+        },
+      ],
+    }
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.balance-block {
+  .balance-section {
+    font-size: 24px;
+    font-weight: 700;
+  }
+}
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,61 +1,59 @@
 <template>
-  <div>
-    <b-container class="py-5">
-      <b-row class="justify-content-center">
-        <b-col cols="6">
-          <BlockSection class="price-block">
-            <h1 class="price-section">
-              MOON = <span data-atd="moon-price-label">50</span> THBT
-            </h1>
-            <h3 class="balance-section">
-              You have <span data-atd="balance-label">100</span> THBT
-            </h3>
-          </BlockSection>
+  <b-container class="py-5">
+    <b-row class="justify-content-center">
+      <b-col cols="6">
+        <BlockSection class="price-block">
+          <h1 class="price-section">
+            MOON = <span data-atd="moon-price-label">50</span> THBT
+          </h1>
+          <h3 class="balance-section">
+            You have <span data-atd="balance-label">100</span> THBT
+          </h3>
+        </BlockSection>
 
-          <BlockSection>
-            <form>
-              <b-form-group label="Amount to buy (THBT)" label-for="thbtInput">
-                <b-form-input
-                  id="thbtInput"
-                  v-model="thbtInput"
-                  data-atd="thbt-input"
-                />
-              </b-form-group>
+        <BlockSection>
+          <form>
+            <b-form-group label="Amount to buy (THBT)" label-for="thbtInput">
+              <b-form-input
+                id="thbtInput"
+                v-model="thbtInput"
+                data-atd="thbt-input"
+              />
+            </b-form-group>
 
-              <b-form-group label="Amount MOON" label-for="moonInput">
-                <b-form-input
-                  id="moonInput"
-                  v-model="moonInput"
-                  data-atd="moon-input"
-                />
-              </b-form-group>
+            <b-form-group label="Amount MOON" label-for="moonInput">
+              <b-form-input
+                id="moonInput"
+                v-model="moonInput"
+                data-atd="moon-input"
+              />
+            </b-form-group>
 
-              <b-form-group
-                label="Slippage Tolerance (%)"
-                label-for="slippageInput"
-              >
-                <b-form-input
-                  id="slippageInput"
-                  v-model="slippageInput"
-                  data-atd="slippage-input"
-                />
-              </b-form-group>
+            <b-form-group
+              label="Slippage Tolerance (%)"
+              label-for="slippageInput"
+            >
+              <b-form-input
+                id="slippageInput"
+                v-model="slippageInput"
+                data-atd="slippage-input"
+              />
+            </b-form-group>
 
-              <b-button
-                type="submit"
-                size="lg"
-                pill
-                variant="primary"
-                data-atd="buy-btn"
-              >
-                Buy
-              </b-button>
-            </form>
-          </BlockSection>
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
+            <b-button
+              type="submit"
+              size="lg"
+              pill
+              variant="primary"
+              data-atd="buy-btn"
+            >
+              Buy
+            </b-button>
+          </form>
+        </BlockSection>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
